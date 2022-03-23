7ed111a62508b41189a73a2200919e8deaa88cc1
tryCod3 <nthuy1999kadd1@gmail.com>
2022-03-23 09:22:07 +0700

    remove todo and setStyle Component Comment

--- myapp/src/components/Comments/CommentPages.js
+++ myapp/src/components/Comments/CommentPages.js
@@ -39,8 +39,12 @@ const CommentPages = ({ name, id }) => {
 	}, []);
 
 	return (
-		<div>
-			<h1>Comment List</h1>
+		<div
+			style={{
+				marginLeft: "-50px",
+				height: "800px",
+				overflow: "auto",
+			}}>
 			{listComments.length === 0 && (
 				<svg viewBox="25 25 50 50">
 					<circle cx="50" cy="50" r="20"></circle>
